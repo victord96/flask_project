4b0dfea72200d02ba7650815603579529a632eb4
--replace-all <victor1996d@gmail.com>
2021-09-22 12:10:55 +0200

    Added flask_wtf and updated username

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, url_for
 from flask.globals import session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
-app.config['SECRET_KEY'] = 'SUPER SECRETO'
+app.config['SECRET_KEY'] = 'SUPER SECRET'
 
 todo = ['Buy Coffee', 'Send purchase request', 'Give video to the producer']
+
+class LoginForm(FlaskForm):
+    username = StringField('User Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Send')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -26,14 +34,24 @@ def index():
 
     return response
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {
         'user_ip': user_ip,
         'todo': todo,
+        'login_form': login_form,
+        'username': username
     }
 
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        session['username'] = username
+
+        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
     

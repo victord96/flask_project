3a131410342a49bf9ebe83b5bf87653a3e7e36a0
--replace-all <victor1996d@gmail.com>
2021-09-22 11:17:42 +0200

    Implemented bootstrap and encoding info

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from flask import Flask, request, make_response, redirect, render_template
+from flask.globals import session
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
 
 todo = ['Buy Coffee', 'Send purchase request', 'Give video to the producer']
 
@@ -17,13 +22,13 @@ def index():
     user_ip = request.remote_addr
 
     response = make_response(redirect('/hello'))
-    response.set_cookie('user_ip', user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 @app.route('/hello')
 def hello():
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
     context = {
         'user_ip': user_ip,
         'todo': todo,

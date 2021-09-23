bb73e0abccb60c705a656028d5b704f1f804617e
--replace-all <victor1996d@gmail.com>
2021-09-23 18:28:56 +0200

    Integrate Blueprints in our app

--- main.py
+++ main.py
@@ -1,22 +1,12 @@
-from flask import Flask, request, make_response, redirect, session, render_template, url_for, flash
-from flask.globals import session
-from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask import request, make_response, redirect, session, render_template, url_for, flash
 import unittest
+from app import create_app
+from app.forms import LoginForm
 
-app = Flask(__name__)
-bootstrap = Bootstrap(app)
 
-app.config['SECRET_KEY'] = 'SUPER SECRET'
+app = create_app()
 
 todo = ['Buy Coffee', 'Send purchase request', 'Give video to the producer']
-
-class LoginForm(FlaskForm):
-    username = StringField('User Name', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Send')
 
 @app.cli.command()
 def test():
@@ -40,26 +30,16 @@ def index():
 
     return response
 
-@app.route('/hello', methods=['GET', 'POST'])
+@app.route('/hello', methods=['GET'])
 def hello():
     user_ip = session.get('user_ip')
-    login_form = LoginForm()
     username = session.get('username')
 
     context = {
         'user_ip': user_ip,
         'todo': todo,
-        'login_form': login_form,
         'username': username
     }
-
-    if login_form.validate_on_submit():
-        username = login_form.username.data
-        session['username'] = username
-
-        flash('Registered user successfully!')
-
-        return redirect(url_for('index'))
 
     return render_template('hello.html', **context)
     
